c7c119603a8d89ee8cf66a76023dfed4897f685b
namisumida <sumida.nami@gmail.com>
2018-11-12 23:01:42 -0500

    Making spacing dynamic

--- js/build.js
+++ js/build.js
@@ -1,11 +1,12 @@
 var svg = d3.select("#svg-barchart");
 var w_svg = document.getElementById('svg-barchart').getBoundingClientRect().width; // get width and height based on window size
+var max_width = 720;
 
 // Layout
-var margin = { top:10, bottom:10, left:10, right:10 };
+var margin = { top:5, bottom:10, left:10, right:5 };
 
 // Within columns
-var dim_col = { w_col:(w_svg-margin.left-margin.right)/3, w_names:80, btwn_colnames:5, btwn_col:10,
+var dim_col = { w_col:(w_svg-margin.left-margin.right)/3, w_names:80, btwn_colnames:5, btwn_col:(max_width-(max_width-w_svg))/100*2,
                    h_col:12, h_btwn:5,
                    top:30, left:5 }
 var w_bars = dim_col.w_col - dim_col.left - dim_col.w_names - dim_col.btwn_colnames;
